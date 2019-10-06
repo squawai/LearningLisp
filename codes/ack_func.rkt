977821be9c9d2910cf9ad55aa210db2fd6be26cb
quawai <quawai@me.com>
2019-10-07 00:11:56 +0900

    tired

---
+++ codes/ack_func.rkt
@@ -0,0 +1,19 @@
+;; a practice to define an Ackermann function
+
+(define (suc n)
+  (+ n 1))
+  
+(define (ack m n)
+  (cond
+    ((= m 0) (suc n))
+    ((and (> m 0) (= n 0)) (ack (- m 1) 1))
+    ((and (> m 0) (> n 0)) (ack (- m 1) (ack m (- n 1))))
+    )
+  )
+
+(display "Enter m: ")
+(set m (read))
+(display "Enter n: ")
+(set n (read))
+
+(ack m n)
